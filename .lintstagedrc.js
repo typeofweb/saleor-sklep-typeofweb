0c4bf790e6787920f1074d90ee83f2ee577d9424
Michal Miszczyszyn <michal@mmiszy.pl>
2022-07-04 20:39:50 +0200

    Add husky and lint-staged

---
+++ .lintstagedrc.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+const buildEslintCommand = (filenames) =>
+	`next lint --fix --file ${filenames
+		.map((f) => path.relative(process.cwd(), f))
+		.join(' --file ')}`;
+
+module.exports = {
+	'*.{js,jsx,ts,tsx}': [buildEslintCommand],
+	'*.{js,jsx,ts,tsx,md,mdx,graphql,yml,yaml,css,scss,json}': [
+		'yarn prettier --write',
+	],
+};
