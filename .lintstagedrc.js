306d7c56eabaf22ddc5f4471886e248c9a25de7d
Michal Miszczyszyn <michal@mmiszy.pl>
2022-07-04 20:48:22 +0200

    Fix build

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -6,6 +6,7 @@ const buildEslintCommand = (filenames) =>
 		.join(' --file ')}`;
 
 module.exports = {
+	'*.{ts,tsx}': 'yarn tsc --noEmit',
 	'*.{js,jsx,ts,tsx}': [buildEslintCommand],
 	'*.{js,jsx,ts,tsx,md,mdx,graphql,yml,yaml,css,scss,json}': [
 		'yarn prettier --write',
