287b261eae250521dcd1c7997a61ecf3f18f05c9
Michal Miszczyszyn <michal@mmiszy.pl>
2022-07-13 16:41:19 +0200

    Remove rewrites

--- next.config.js
+++ next.config.js
@@ -10,12 +10,6 @@ const nextConfig = {
 		domains: ['typeofweb.eu.saleor.cloud'],
 		formats: ['image/avif', 'image/webp'],
 	},
-	rewrites: async () => [
-		{
-			source: '/',
-			destination: '/1',
-		},
-	],
 	experimental: { images: { allowFutureImage: true } },
 	i18n: {
 		defaultLocale: 'pl',
